7d776938ce448f4853c09250144f502048d9628e
Jhonathan Burrel <jhonathanaby@gmail.com>
2025-11-04 08:35:26 -0500

    Interctividad con jv

--- HTML-CSS/script.js
+++ HTML-CSS/script.js
@@ -1,9 +1,12 @@
-const botones = document.querySelectorAll('.button-apply-job');
 
-botones.forEach(boton => {
-    boton.addEventListener('click', () => {
-        boton.textContent = 'Aplicado'
-        boton.classList.add('is-applied')
-        boton.disabled = true
-})
+const jobsListingsSection = document.querySelector('.jobs-listings')
+
+jobsListingsSection.addEventListener('click', (event) => {
+    const element = event.target
+
+    if(element.classList.contains('button-apply-job')){
+        element.textContent = 'Aplicado'
+        element.classList.add('is-applied')
+        element.disabled = true
+    }
 })
